8f40f8e350728899200fd5528e78e209884c70bf
pietroDeAngeli <pietro.deangeli@studenti.unitn.it>
2025-07-12 11:17:27 +0200

    changed structure

--- face_detection/faceParser.py
+++ face_detection/faceParser.py
@@ -1,26 +1,8 @@
 # Tools
-import os
 import tdqm as tqdm
 
 # Custom Libraries
 import face_detection_tools as tools
-
-# Get all the videos
-def get_dir_videos(path):
-    """
-    Retrieve all .mp4 video file paths from a given directory.
-
-    Parameters:
-        path (str): Path to the directory containing video files.
-
-    Returns:
-        list of str: List of full paths to .mp4 video files.
-    """
-    videos = []
-    for file in os.listdir(path):
-        if file.endswith(".mp4"):
-            videos.append(os.path.join(path, file))
-    return videos
 
 # Function to perform face detection on a list of videos
 def faces_detection(videos):
